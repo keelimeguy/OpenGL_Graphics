27f0e4933ec6178a3eb872d030ab110a85b1670e
Keelin Wheeler <keelinwheeler@gmail.com>
2018-03-13 17:44:35 -0400

    Adding GLEW library and drawing a basic triangle

--- OpenGL_Graphics/src/Application.cpp
+++ OpenGL_Graphics/src/Application.cpp
@@ -1,4 +1,7 @@
+#include <GL\glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main() {
 	GLFWwindow* window;
@@ -6,7 +9,7 @@ int main() {
 	if (!glfwInit())
 		return -1;
 
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "OpenGL_Testing", NULL, NULL);
 	if (!window) {
 		glfwTerminate();
 		return -1;
@@ -14,8 +17,30 @@ int main() {
 
 	glfwMakeContextCurrent(window);
 
+	if (glewInit() != GLEW_OK)
+		return -1;
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	float positions[6] = {
+		-0.5f, -0.5f,
+		0.0f, 0.5f,
+		0.5f, -0.5f
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+	
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
+	
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
